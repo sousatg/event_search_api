d48ddc261f2779f69e32ca1ba8fac6fa9131754f
Gil Sousa <tiagosousafl@gmail.com>
2023-08-15 20:29:56 +0100

    Create event model

--- src/api/models.py
+++ src/api/models.py
@@ -1,14 +1,13 @@
 from src.api.extensions import db
-from sqlalchemy.types import Float, String, Date, DateTime
 
 
 class Event(db.Model):
     __tablename__ = "events"
-    id = db.Column(String, primary_key=True)
-    title = db.Column(String, nullable=False)
-    start_date = db.Column(Date, nullable=False)
-    start_time = db.Column(DateTime, nullable=False)
-    end_date = db.Column(Date, nullable=False)
-    end_time = db.Column(DateTime, nullable=False)
-    min_price = db.Column(Float, nullable=False)
-    max_price = db.Column(Float, nullable=False)
+    id = db.Column(db.String, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    start_date = db.Column(db.Date, nullable=False, index=True)
+    start_time = db.Column(db.Time, nullable=False, index=True)
+    end_date = db.Column(db.Date, nullable=False, index=True)
+    end_time = db.Column(db.Time, nullable=False, index=True)
+    min_price = db.Column(db.Float, nullable=False)
+    max_price = db.Column(db.Float, nullable=False)
