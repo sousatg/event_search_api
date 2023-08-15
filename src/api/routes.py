28a510e9177e830ef475f8aba1226708040c8a8b
Gil Sousa <tiagosousafl@gmail.com>
2023-08-15 20:30:29 +0100

    Add search functionality

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request, jsonify
-from api.schemas import SearchSchema
+from api.schemas import SearchSchema, EventSchema
+from api.models import Event
+from datetime import datetime
 
 
 bp = Blueprint('main', __name__)
@@ -21,7 +23,28 @@ async def search_events():
                 "code": "INVALID_INPUT",
                 "message": "INVALID_INPUT"
             },
-            "data": errors
+            "data": None
         }), 400
 
-    return {}
+    starts_at = request.args.get("starts_at", None)
+    ends_at = request.args.get("ends_at", None)
+
+    start_date = datetime.strptime(starts_at, "%Y-%m-%dT%H:%M:%SZ")
+    start_time = datetime.strptime(starts_at, "%Y-%m-%dT%H:%M:%SZ").time()
+    end_date = datetime.strptime(ends_at, "%Y-%m-%dT%H:%M:%SZ")
+    end_time = datetime.strptime(ends_at, "%Y-%m-%dT%H:%M:%SZ").time()
+
+    all_events = Event.query.filter(
+        Event.start_date >= start_date,
+        Event.start_time >= start_time,
+        Event.end_date <= end_date,
+        Event.end_time <= end_time
+    ).all()
+
+    result = EventSchema(many=True).dump(all_events)
+
+    response = {"data": {
+        "events": result
+    }}
+
+    return response
--- src/api/schemas.py
+++ src/api/schemas.py
@@ -3,5 +3,19 @@ from marshmallow import fields
 
 
 class SearchSchema(ma.Schema):
-    starts_at = fields.DateTime()
-    ends_at = fields.DateTime()
+    starts_at = fields.DateTime(required=True, allow_none=False)
+    ends_at = fields.DateTime(required=True, allow_none=False)
+
+
+class EventSchema(ma.Schema):
+    class Meta:
+        fields = (
+            "id",
+            "title",
+            "start_date",
+            "start_time",
+            "end_date",
+            "end_time",
+            "min_price",
+            "max_price"
+        )
